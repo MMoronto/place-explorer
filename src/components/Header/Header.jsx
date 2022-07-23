54bfa0cd8a7c2166f0fa49840dc394f4afdcd98a
MMORONTO <t2wycemo@gmail.com>
2022-07-22 21:15:14 -0500

    squish bug-fix searchbar error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,16 +17,16 @@ const Header = () => {
                 </Typography>
                 <Box display="flex">
                     <Typography variant="h6" className={classes.title}>
-                        Get curious about your neighborhood
+                        Let's Explore
                     </Typography>
-                    {/* <Autocomplete>
-                        <div className={classes.title}>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
-                            <InputBase placeholder="Search..." clases={{ root: classes.inputRoot, input: classes.inputInput}} />
+                            <InputBase placeholder="Search... " classes={{ root: classes.inputRoot, input: classes.inputInput}} />
                         </div>
-                    </Autocomplete> */}
+                    {/* </Autocomplete> */}
                 </Box>
             </Toolbar>
         </AppBar>
