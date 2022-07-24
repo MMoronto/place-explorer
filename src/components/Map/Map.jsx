da653b1d5107b672819b0969c096a30dda9bf6c2
MMORONTO <t2wycemo@gmail.com>
2022-07-23 22:27:15 -0500

    set up map.jsx.import components, material-ui components

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 
 const Map = () => {
     return (
