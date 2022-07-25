ba55137518ba18eb329dee458e3318c6e179c207
MMORONTO <t2wycemo@gmail.com>
2022-07-24 19:01:46 -0500

    define isMobile hook

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from 'Q@material-ui/icons/LocationOutlined';
+import LocationOutlinedIcon from '@material-ui/icons/locationOutlined';
 import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
 const Map = () => {
     const classes = useStyles();
-    
+    const isMobile = useMediaQuery('(min-width:600px)');
+
     return (
         <h1>Map</h1>
     );
