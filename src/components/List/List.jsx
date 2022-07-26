5f9c2dd287c5a1f5eadb2537ad34756120f8e212
MMORONTO <t2wycemo@gmail.com>
2022-07-26 14:47:30 -0500

    define useState hook and event handlers for menu items

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,14 +5,17 @@ import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
+    const [type, setType] = useState('restaurants');
 
     return (
         <div className={classes.container}>
             <Typography variant='h4'>Essential Amenities near you</Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
-                <Select value={} onChange={}>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
                     <MenuItem value='Restaurants'>Restaurants</MenuItem>
+                    <MenuItem value='Hotels'>Hotels</MenuItem>
+                    <MenuItem value='Museums'>Museums</MenuItem>
                 </Select>
             </FormControl>
         </div>
