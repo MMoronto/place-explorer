5665111c791efdbee8ac2ecfdc829173a140f2ce
MMORONTO <t2wycemo@gmail.com>
2022-07-26 11:35:05 -0500

    setup map component page elements/format

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
+import useStyles from './styles';
+
 const List = () => {
+    const classes = useStyles();
+
     return (
-        <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant='h4'>Essential Amenities near you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={} onChange={}></Select>
+            </FormControl>
+        </div>
     );
 }
 
