310d8bf8a369a46b1db164f3099674618307b65a
MMORONTO <t2wycemo@gmail.com>
2022-07-25 22:16:22 -0500

    debug map render issue

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
-import useStyles from './styles.js';
+import useStyles from './styles';
 
 const Map = () => {
     const classes = useStyles();
@@ -15,7 +15,7 @@ const Map = () => {
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: '' }}
+                bootstrapURLKeys={{ key: 'AIzaSyCdFCmebvCSKBs2l7JDA1eNry-SNpCpzPU' }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
