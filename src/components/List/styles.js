8d4fc700d3ab6a6961908ee45d37552b221fd292
MMORONTO <t2wycemo@gmail.com>
2022-07-26 11:26:53 -0500

    setup & define style params for List.js component

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,9 +4,19 @@ export default makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
     },
-    selectEMpty: {},
-    loading: {},
-    container: {},
-    marginBottom: {},
-    list: {},
+    selectEMpty: {
+        marginTop: theme.spacing(2),
+    },
+    loading: {
+        height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+    },
+    container: {
+        padding: '25px',
+    },
+    marginBottom: {
+        marginBottom: '30px',
+    },
+    list: {
+        height: '75vh', overflow: 'auto',
+    },
 }));
