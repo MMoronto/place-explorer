84439e21a748ccdad6af2723675fdd6224790e72
MMORONTO <t2wycemo@gmail.com>
2022-07-26 21:38:46 -0500

    get API params from travel-advisor api

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,41 @@
 import axios from "axios";
 
+
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'X-RapidAPI-Key': '41599fd560msh907746e088eb965p1a2b46jsnb4b6b8c5b300',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 const getPlacesData = async () => {
     try {
+        const response = await axios.get();
 
     } catch (error) {
-        
+
     }
 }
