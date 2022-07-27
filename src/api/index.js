7d42c4dc7ec9dcad64ac6532a2758f004fc786d2
MMORONTO <t2wycemo@gmail.com>
2022-07-26 21:58:56 -0500

    setup , test api call with static coordinates

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,13 @@
 import axios from "axios";
 
-
-import axios from "axios";
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 const options = {
-  method: 'GET',
-  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
   params: {
     bl_latitude: '11.847676',
     tr_latitude: '12.838442',
     bl_longitude: '109.095887',
     tr_longitude: '109.149359',
-    restaurant_tagcategory_standalone: '10591',
-    restaurant_tagcategory: '10591',
-    limit: '30',
-    currency: 'USD',
-    open_now: 'false',
-    lunit: 'km',
-    lang: 'en_US'
   },
   headers: {
     'X-RapidAPI-Key': '41599fd560msh907746e088eb965p1a2b46jsnb4b6b8c5b300',
@@ -25,17 +15,13 @@ const options = {
   }
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
 
-const getPlacesData = async () => {
+export const getPlacesData = async () => {
     try {
-        const response = await axios.get();
+        const { data: { data } } = await axios.get(URL, options);
 
+        return response;
     } catch (error) {
-
+        console.log(error)
     }
 }
