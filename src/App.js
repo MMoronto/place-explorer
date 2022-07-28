e6a63478b0e3d281b21735fa7697ab93517da413
MMORONTO <t2wycemo@gmail.com>
2022-07-28 16:50:38 -0500

    set coordinates to current user location

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import Map from './components/Map/Map';
 const App = () => {
     const [places, setPlaces] = useState([]);
 
-    const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+    const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState(null);
 
     useEffect(() => {
-
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, logitude} }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
     }, []);
 
     useEffect(() => {
