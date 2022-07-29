5621a9191af49b42779ccc80601c113458fa11dc
MMORONTO <t2wycemo@gmail.com>
2022-07-29 13:04:42 -0500

    fix map rendering issue

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,20 @@ import axios from "axios";
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Key': '41599fd560msh907746e088eb965p1a2b46jsnb4b6b8c5b300',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-
 export const getPlacesData = async (sw, ne) => {
     try {
-        const { data: { data } } = await axios.get(URL, options);
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'x-rapidapi-key': '41599fd560msh907746e088eb965p1a2b46jsnb4b6b8c5b300',
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+            }
+          });
 
         return data;
     } catch (error) {
