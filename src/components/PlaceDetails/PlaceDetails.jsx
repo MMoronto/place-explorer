3bae4904c6c584e9b4ef4c3878483bfbc8835de5
MMORONTO <t2wycemo@gmail.com>
2022-08-02 12:14:56 -0500

    set up place details for rendering cards for each  item on the list component

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const PlaceDetails = ({ place }) => {
+    console.log(place);
+
     return (
         <h1>{place.name}</h1>
     );
