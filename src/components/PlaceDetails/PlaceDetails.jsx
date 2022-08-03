eff8b06d5b17e69522821af78556777e69b033a8
MMORONTO <t2wycemo@gmail.com>
2022-08-03 10:12:05 -0500

    import files & dependencies into placeDetails component

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import phoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
 const PlaceDetails = ({ place }) => {
     console.log(place);
