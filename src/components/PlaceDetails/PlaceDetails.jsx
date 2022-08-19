f7f350b23bc870f31d5cbfe462100c89d394fc6e
MMORONTO <t2wycemo@gmail.com>
2022-08-18 23:15:12 -0500

    add additional details to box component

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -19,7 +19,8 @@ const PlaceDetails = ({ place }) => {
             <CardContent>
                 <Typography gutterBottom variant="h5">{place.name}</Typography>
                 <Box display="flex" justifyContent="space-between">
-                    <Typography variant="subtitle1"></Typography>
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
                 </Box>
             </CardContent>
 
