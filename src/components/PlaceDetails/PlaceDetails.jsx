45755922a6d530ba0a53e981cbf17b51a465ee46
MMORONTO <t2wycemo@gmail.com>
2022-08-23 23:39:45 -0500

    fix typo. debug

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -33,7 +33,7 @@ const PlaceDetails = ({ place }) => {
                         <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
                     </Box>
                 ))}
-                {place?.cuisine.map(({ name }) => (
+                {place?.cuisine?.map(({ name }) => (
                     <Chip key={name} size="small" label={name} className={classes.chip} />
                 ))}
             </CardContent>
