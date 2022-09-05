1bd7ce65dd097b6029a84ddcf713e03c4c37e193
MMORONTO <t2wycemo@gmail.com>
2022-09-05 08:03:20 -0500

    Add type to dependency array

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
             setPlaces(data);
             setIsLoading(false);
         })
-    }, [coordinates, bounds]);
+    }, [type, coordinates, bounds]);
 
     return (
         <>
