4301dba4503999d794cd2fbebb414226fce5bd36
MMORONTO <t2wycemo@gmail.com>
2022-10-25 22:38:37 -0500

    debug map.jsx render issue

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
 import mapStyles from './mapStyles';
-import useStyles from './styles';
+import useStyles from './styles.js';
 
 const handleApiLoaded = (map, maps) => {
 
